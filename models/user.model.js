2b8a41829ffc8fed15060edaed2fce43de56f881
somnath das <somnathdas9771@gmail.com>
2023-07-04 21:45:20 +0530

    for trial

---
+++ models/user.model.js
@@ -0,0 +1,100 @@
+/**
+ * this will hold the schema for the users
+ * it explain the different fields of user and how it will be stored in the mongodb
+ 
+ */
+/*const mongoose=require('mongoose');
+
+const userSchema= new mongoose.Schema({
+
+//field name is name,userId,here we define type that is String(it is a type)
+//every field has constraints which is mandatory so required:true is a constraints
+// below we are defining Schema
+    name:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:10,
+        lowercase:true
+    },
+    userType:{
+        type:String,
+        required:true,
+        default:"CUSTOMER",
+        enum:["CUSTOMER","ADMIN"]
+    },
+
+},{timestamps:true}); // Timestamps will define creation time and updation time in schema that is why it is used.
+/**
+ * Define the collection name where it will be stored
+ * here collection name is #User,  telling that store whatever field present in #userSchema should be there in collection
+ * below line create collection i.e - User.
+ */
+
+ /**
+  * 
+  * mongoose.model("User",userSchema); this line is creating document with name User but we have to store as module */
+//module.exports= mongoose.model("User",userSchema);//want whole file to be available as a module so store as module.exports
+
+/**
+ * This will hold the schema for the user
+ * 
+ * It explains the different fields of use and how it will be
+ * stored in the mongodb
+ */
+
+ const mongoose = require('mongoose');
+
+
+ const userSchema = new mongoose.Schema({
+ 
+    name : {
+     type : String,
+     required : true
+    },
+    userId : {
+     type : String,
+     required : true,
+     unique : true
+    },
+    password : {
+     type : String,
+     required : true
+    },
+    email : {
+      type : String,
+      required : true,
+      unique : true,
+      minLength : 10,
+      lowercase : true
+ 
+    },
+    userType : {
+     type : String,
+     required : true,
+     deafult : "CUSTOMER",
+     enum : ["CUSTOMER", "ADMIN"]
+    }
+    
+ 
+ },{timestamps : true});
+ 
+ /**
+  * Define the collection name where it will be stored
+  */
+ 
+ module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -1,10 +1,62 @@
-const express  = require('express');
+/*const express  = require('express');
 const serverConfig =require('./configs/server.config');
+const mongoose=require('mongoose');
+const dbConfig= require('./configs/db.config');
 
 
 const app = express();
+/**
+ * logic to connect to mongoDB and create an Admin user
+ * need to have a mongoDB up and running in local machine
+ */
+/*mongoose.connect("dbConfig.DB_URL");
+const db=mongoose.connection;
+
+db.on("error",()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open",()=>{
+    console.log("DB is connected");
+});
+
+
+
+app.listen(serverConfig.PORT, ()=>{
+    console.log(`server started on the port number ${serverConfig.PORT}` );
+})*/
+
+
+
+
+const express  = require('express');
+const serverConfig =require('./configs/server.config');
+const mongoose = require('mongoose');
+const dbConfig = require('./configs/db.config');
+const userModel = require('./models/user.model');
+//const bcrypt = require('bcrypt');
+
+const app = express();
+
+
+/**
+ * Logic to connect to MongoDB and create an ADMIN user
+ * Need to have the mongodb up and running in your local machine
+ */
+mongoose.connect(dbConfig.DB_URL);
+const db = mongoose.connection ;
+
+db.on("error", ()=>{
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("DB is connected");
+});
+
 
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server started on the port number ${serverConfig.PORT}` );
 })
+
